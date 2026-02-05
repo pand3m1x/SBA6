343ba4896a523cb2fe5f033d3895e0d601bc3fd4
TheRedQueen <pand3m1x@gmail.com>
2026-02-05 15:53:05 -0500

    ‧₊ ♪˚⊹ Grabbed code from mod 6 inventory tracker and placed it into Product.TS ‧₊ ♪˚⊹

--- e-commerce-system/src/models/Product.ts
+++ e-commerce-system/src/models/Product.ts
@@ -5,3 +5,28 @@
 // on data provided in the API response.
 // Include methods displayDetails() and getPriceWithDiscount(), 
 // and implement them appropriately based on the provided data.
+
+//I just took this from mod-6 inventory tracker and modified it for e-commerce system
+
+export class Product { // lesson 3
+  static taxRate = 0.10; // taxRate isn't registering correctly
+
+  sku: string;
+  name: string;
+  price: number;
+
+constructor(sku: string, name: string, price: number,) {
+  this.sku = sku;
+  this.name = name;
+  this.price = price;
+}
+
+  displayDetails(): string {
+  return `${this.name} costs $${this.price}.`;
+  }
+
+  getPriceWithTax(): number { 
+    return this.price * (Product.taxRate + 1);
+  //10% tax
+  }
+}
