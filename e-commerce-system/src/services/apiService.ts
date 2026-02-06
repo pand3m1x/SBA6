236e8ff36ac0bf3936c2cca9c4731f6fb16d15e2
TheRedQueen <pand3m1x@gmail.com>
2026-02-05 21:51:49 -0500

    Grabbed API link, not sure which file to put it in, may need to revisit errors to be more specific to API‧₊ ♪˚⊹

--- e-commerce-system/src/services/apiService.ts
+++ e-commerce-system/src/services/apiService.ts
@@ -1,5 +1,27 @@
-// Handle Asynchronous Operations:
+// Link https://dummyjson.com/docs/products#products-limit_skip
 
 // API Service (apiService.ts):
 // Create API requests using async/await and Promises.
 // Implement functions to fetch product data and handle errors using try/catch.
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(console.log);
+
+import {Product} from './Product'
+
+// export async function handleData() { //Is async okay, or should it be promise chains...
+//   try {
+//     const product = await getProductDetails();
+//     console.log("Fetched product:", product);
+
+//     const reviews = await getProductReviews(product);
+//     console.log("Fetched reviews:", reviews);
+
+//     const relatedProducts = await getRelatedProducts(reviews);
+//     console.log("Fetched related products:", relatedProducts);
+//   } catch (error) { //would I want an error for each variable?
+//     console.error("Error fetching data:", error);
+//   }
+// }
+
+// handleData();
--- e-commerce-system/src/utils/discountCalculator.ts
+++ e-commerce-system/src/utils/discountCalculator.ts
@@ -5,3 +5,4 @@
 // This function should return the dollar amount that a 
 // product is discounted by. For example, if a product costs $100 
 // and has a 10% discount, the function should return $10.
+
