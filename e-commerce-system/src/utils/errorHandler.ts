cf5fa166e0ee401fd73de90747f916a6ea18923c
TheRedQueen <pand3m1x@gmail.com>
2026-02-05 21:31:58 -0500

    ‧₊ ♪˚⊹  Added Errors to errorHandler ‧₊ ♪˚⊹

--- e-commerce-system/src/utils/errorHandler.ts
+++ e-commerce-system/src/utils/errorHandler.ts
@@ -3,3 +3,22 @@
 // Error Handler Module (errorHandler.ts):
 // Implement a custom error class and functions to handle
 //  different types of errors gracefully.
+
+//quanity less than 1
+class ValidationError extends Error { //lesson 5
+  message:string
+
+  constructor (message:string){
+    super(message)
+    this.name = "ValidationError";
+  }
+}
+//price negative number
+class PaymentError extends Error {
+  message:string
+
+  constructor (message:string){
+    super(message)
+    this.name = "PaymentError"
+  }
+}
